e93841040e6b32bd5aee89073ca64b794551c0a1
OlenaMyroshnykova <elenamirespania@gmail.com>
2024-11-08 23:18:32 +0100

    Added new mettods

--- list/src/main/java/com/list/HandlerWeekDays.java
+++ list/src/main/java/com/list/HandlerWeekDays.java
@@ -1,6 +1,7 @@
 package com.list;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class HandlerWeekDays {
@@ -14,6 +15,33 @@ public class HandlerWeekDays {
         return days;
     }
 
+    public int getDaysCount() {
+        return days.size();
+    }
+    public boolean removeDay(String day) {
+        return days.remove(day);
+    }
+
+    public String getDay(int index) {
+        if (index >= 0 && index < days.size()) {
+            return days.get(index);
+        } else {
+            throw new IndexOutOfBoundsException("Índice fuera de rango.");
+        }
+    }
+
+    public boolean dayExists(String day) {
+        return days.contains(day);
+    }
+
+    public void sortDaysAlphabetically() {
+        Collections.sort(days);
+    }
+
+    public void clearDays() {
+        days.clear();
+    }
+    
     public void setDays(List<String> days) {
     this.days = days;
     }
