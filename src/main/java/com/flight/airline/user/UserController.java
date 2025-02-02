cdd8730945c6ceadbb042f0f4db23c7d462fb29c
OlenaMyroshnykova <elenamirespania@gmail.com>
2025-02-02 19:30:25 +0100

    fixed avatar saving

--- src/main/java/com/flight/airline/user/UserController.java
+++ src/main/java/com/flight/airline/user/UserController.java
@@ -53,13 +53,20 @@ public class UserController {
 
     @PostMapping("/upload-avatar")
     public ResponseEntity<?> uploadAvatar(@RequestParam("file") MultipartFile file, Principal principal) {
-    try {
-        String fileName = fileStorageService.saveFile(file, principal.getName());
-        return ResponseEntity.ok(Map.of("message", "Avatar uploaded successfully!", "fileName", fileName));
-    } catch (Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("message", "File upload failed!"));
+        try {
+            String fileName = fileStorageService.saveFile(file, principal.getName());
+
+            User user = userRepository.findByUsername(principal.getName())
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+
+            user.setAvatarUrl("/uploads/avatars/" + fileName);
+            userRepository.save(user);
+
+            return ResponseEntity.ok(Map.of("message", "Avatar uploaded successfully!", "fileName", fileName, "avatarUrl", user.getAvatarUrl()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("message", "File upload failed!"));
+        }
     }
-}
 
     @GetMapping("/profile")
     public ResponseEntity<Map<String, Object>> getUserProfile() {
